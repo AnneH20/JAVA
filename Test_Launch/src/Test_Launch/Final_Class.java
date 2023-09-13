e98785dd2b2d677e31f2d27f689d73b2674231cf
AnneH20 <alh1310@msstate.edu>
2023-09-13 13:30:07 -0500

    Final Class Example Start

---
+++ Test_Launch/src/Test_Launch/Final_Class.java
@@ -0,0 +1,22 @@
+package Test_Launch;
+
+import java.util.Scanner;
+
+public class Final_Class {
+
+	public static void main(String[] args) {
+		
+		int min = 0;
+		int max = 0;
+		
+		Scanner keyboard_input = new Scanner(System.in);
+		int [] array1 = new int[5];
+		for (int i=0; i<array1.length; i++) {
+			array1[i] = keyboard_input.nextInt();
+		}
+		
+		
+		
+	}
+
+}
